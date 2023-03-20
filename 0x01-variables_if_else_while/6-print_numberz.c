52a7e2b1620b47051cdfe90170e37979862371e2
olaashraf431 <olaashraf431@gmail.com>
2023-03-20 15:28:00 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+i#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - prints single number
+*
+*Return: 0 (Sucess)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar('\n');
+	return (0);
+
+
+}
