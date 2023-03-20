dd396a416d57ea73b7a8e58afdf6179607fa28e4
olaashraf431 <olaashraf431@gmail.com>
2023-03-20 07:11:21 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,14 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main- enter point
- * Description : print alphabet in uppercase
- * return: 0(sucess)
-*/
+ * main - print the alphabet
+ *
+ * Return: 0 always (Sucess)
+ */
 int main(void)
 {
 	char ch[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i = 0;
+
 	while (i <= 25)
 	{
 		putchar(ch[i]);
