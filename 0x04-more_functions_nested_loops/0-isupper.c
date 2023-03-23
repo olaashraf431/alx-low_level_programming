01870fda2ae84be3b5b285ab05ea18e7f90c839a
olaashraf431 <olaashraf431@gmail.com>
2023-03-23 16:00:16 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _is upper - check if c is upper
+ *
+ * Return: Always 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
