b15912d467d75a61f18720d9c4b99131f54bbd69
olaashraf431 <olaashraf431@gmail.com>
2023-03-23 16:20:43 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * _is upper - check if c is upper
+ * _isupper - check if c is upper
  *
- * Return: Always 0
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase. 0 if not
 */
 
 int _isupper(int c)
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _**** - check if c is digit
+ *
+ * Return: Always 0
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+
+}
