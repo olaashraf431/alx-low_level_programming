a646a6cc8fe7b313ff363bf19cbbd5dee5beaefc
olaashraf431 <olaashraf431@gmail.com>
2023-03-27 17:16:22 -0700

    4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
 int _putchar(char c);
-void reset_to_98.c(int *n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
